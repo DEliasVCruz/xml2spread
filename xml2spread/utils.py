5463b2b6cc8c919074b6df31a2eeb177fb78db59
DEliasVilelaC <danielvilelayo@gmail.com>
2022-09-02 19:24:20 -0500

    feat(utils): add initial imports

---
+++ xml2spread/utils.py
@@ -0,0 +1,10 @@
+import xml.etree.ElementTree as ET
+from datetime import datetime
+from typing import List
+
+namespaces = {
+    "cbc": "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2",
+    "cac": "urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2",
+}
+
+
