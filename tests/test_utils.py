d08af30f7c00d11965751a195b6a7af11da6952b
DEliasVilelaC <danielvilelayo@gmail.com>
2022-09-02 19:23:16 -0500

    test(utils): add fixture setup

---
+++ tests/test_utils.py
@@ -0,0 +1,31 @@
+import re
+import xml.etree.ElementTree as ET
+from pathlib import Path
+
+import pytest as pt
+
+from xml2spread.utils import get_fecha_factura, get_id_factura, get_item_factura
+
+roots = []
+ids = []
+
+fechas = [
+    "30/07/2022",
+    "22/07/2022",
+    "11/07/2022",
+    "22/07/2022",
+    "22/07/2022",
+    "30/07/2022",
+    "14/07/2022",
+    "11/07/2022",
+    "09/07/2022",
+    "09/07/2022",
+]
+
+for file in Path().cwd().joinpath("tests", "fixtures", "xml_facturas").iterdir():
+    root = ET.parse(file).getroot()
+    factura_id = re.findall(r"E001-\d{4}", file.name)
+    ids.append(factura_id[0])
+    roots.append(root)
+
+
