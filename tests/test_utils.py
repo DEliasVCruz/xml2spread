834538670db2a91e6988ca0b3b762cc0229ac959
DEliasVilelaC <danielvilelayo@gmail.com>
2022-10-11 19:09:54 -0500

    feat(test): add test for tax capture feature

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,9 +4,15 @@ from pathlib import Path
 
 import pytest as pt
 
-from xml2spread.utils import get_fecha_factura, get_id_factura, get_item_factura
+from xml2spread.utils import (
+    get_tax_info,
+    get_fecha_factura,
+    get_id_factura,
+    get_item_factura,
+)
 
 fc_roots = []
+mnt_roots = []
 ids = []
 
 fechas = [
@@ -22,11 +28,17 @@ fechas = [
     "09/07/2022",
 ]
 
+ammount_values = [(35.61, 6.41, 42.02), (5796.61, 1043.39, 6840.00), (4766.10, 857.9, 5624.00)]
+
 for file in Path().cwd().joinpath("tests", "fixtures", "xml_facturas").iterdir():
     root = ET.parse(file).getroot()
     factura_id = re.findall(r"E001-\d{4}", file.name)
     ids.append(factura_id[0])
     fc_roots.append(root)
+
+for file in Path().cwd().joinpath("tests", "fixtures", "xml_montos").iterdir():
+    root = ET.parse(file).getroot()
+    mnt_roots.append(root)
 
 
 # @pt.mark.skip
@@ -74,3 +86,13 @@ def test_get_three_item_factura():
     ]
 
     assert output == expected_item
+
+
+# @pt.mark.skip
+@pt.mark.parametrize(
+    "xml_root,expected_ammounts",
+    [(root, ammounts) for root, ammounts in zip(mnt_roots, ammount_values)],
+)
+def test_get_tax_info(xml_root, expected_ammounts):
+    output = get_tax_info(xml_root)
+    assert output == expected_ammounts
