cdc7baa0fcf7f53c241695f21228b4d1679aea2b
Daniel Vilela <dvilela@truora.com>
2023-05-06 19:14:08 -0500

    feat: add support for resume file

--- xml2spread/procedures.py
+++ xml2spread/procedures.py
@@ -6,12 +6,15 @@ from tkinter.filedialog import askopenfilenames, asksaveasfilename
 from tkinter.messagebox import askretrycancel
 from zipfile import ZipFile
 
-from xml2spread.utils import get_fecha_factura, get_id_factura, get_item_factura
+from xml2spread.utils import get_fecha_factura, get_id_factura, get_item_factura, get_tax_info, get_client_name
 
 
-def extract_and_write(files, result_file: tuple[str] | str):
+def extract_and_write(files, result_files_name: tuple[str] | str):
 
     with TemporaryDirectory() as tmpdirname:
+
+        items = []
+        facturas_monto = []
 
         for file in files:
             with ZipFile(file, mode="r") as archive:
@@ -21,19 +24,38 @@ def extract_and_write(files, result_file: tuple[str] | str):
 
         temp_files_dir = Path(tmpdirname)
 
-        with open(str(result_file), mode="w") as file:
+        for file in temp_files_dir.iterdir():
+            root = ET.parse(file).getroot()
+            id = get_id_factura(root)
+            fecha = get_fecha_factura(root)
+            items = get_item_factura(root)
+
+            factura_monto: list[float | str | list] = [id, fecha, get_client_name(root)]
+            factura_monto.extend(list(get_tax_info(root)))
+
+            facturas_monto.append(factura_monto)
+
+            for item in items:
+                item.extend([id, fecha])
+
+        file = Path(str(result_files_name))
+        file = file.parent / file.stem
+        detalle_file = f"{str(file)}_detalle.csv"
+        ammount_file = f"{str(file)}_montos.csv"
+
+        with open(detalle_file, mode="w") as file:
             csv_writer = csv.writer(file, delimiter=",")
 
             csv_writer.writerow(["Descripcion", "Cantidad", "Factura", "Fecha"])
+            for item in items:
+                csv_writer.writerow(item)
 
-            for file in temp_files_dir.iterdir():
-                root = ET.parse(file).getroot()
-                id = get_id_factura(root)
-                fecha = get_fecha_factura(root)
-                items = get_item_factura(root)
-                for item in items:
-                    item.extend([id, fecha])
-                    csv_writer.writerow(item)
+        with open(ammount_file, mode="w") as file:
+            csv_writer = csv.writer(file, delimiter=",")
+
+            csv_writer.writerow(["Factura", "Fecha", "Cliente", "Base Imponible", "IGV", "Total"])
+            for factura in facturas_monto:
+                csv_writer.writerow(factura)
 
 
 def selector(action: str, file_type: str, retry: bool) -> tuple[tuple[str] | str, bool]:
@@ -54,7 +76,7 @@ def selector(action: str, file_type: str, retry: bool) -> tuple[tuple[str] | str
                 (f"Archivos {file_type}", f"*.{file_type}"),
                 ("Todos los archivos", "*.*"),
             ),
-            title="Seleccione archivo a guardar",
+            title="Seleccione nombre para archivos a guardar",
         )
 
     if selection:
--- xml2spread/utils.py
+++ xml2spread/utils.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
 from datetime import datetime
-from typing import List
+from typing import List, Tuple
 
 namespaces = {
     "cbc": "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2",
@@ -39,6 +39,26 @@ def get_item_factura(root: ET.Element) -> List[List]:
             items.append([descripcion, cantidad])
 
     return items
+
+
+def get_tax_info(root: ET.Element) -> Tuple[float, float, float]:
+    tax_elem = root.find("cac:TaxTotal", namespaces=namespaces)
+
+    tax_info = tax_elem.find("cac:TaxSubtotal", namespaces=namespaces) if tax_elem is not None else None
+
+    base_imponible, igv = [float(str(value.text)) for value in tax_info[:2]] if tax_info is not None else (None, None)
+    total = (base_imponible + igv) if (base_imponible is not None) and (igv is not None) else 0
+
+    return (base_imponible or 0, igv or 0, round(number=total, ndigits=2))
+
+
+def get_client_name(root: ET.Element) -> str:
+    customer = root.find("cac:AccountingCustomerParty", namespaces=namespaces)
+    party = customer.find("cac:Party", namespaces=namespaces) if customer is not None else None
+    entity = party.find("cac:PartyLegalEntity", namespaces=namespaces) if party is not None else None
+    name = entity.findtext("cbc:RegistrationName", namespaces=namespaces) if entity is not None else None
+
+    return (name or "")
 
 
 if __name__ == "__main__":
